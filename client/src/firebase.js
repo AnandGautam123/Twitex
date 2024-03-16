3888e791dceb1394c1a6a620f1fb1f03d78cca05
AnandGautam123 <anand0171.be21@chitkara.edu.in>
2024-03-16 18:09:52 +0530

    Twitext upload2 firebase file

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkXpOwo54RcuUPmzs0rV-kwoO5jyXwLEQ",
+  authDomain: "twitex-3b50b.firebaseapp.com",
+  projectId: "twitex-3b50b",
+  storageBucket: "twitex-3b50b.appspot.com",
+  messagingSenderId: "869886846559",
+  appId: "1:869886846559:web:af7730fd9c43b7ad109dea",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
